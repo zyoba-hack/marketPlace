ace6fbf96c9ab99eefbf2bf79122b8a83c0ba798
Suresh <suresh@zyoba.com>
2015-03-06 17:22:56 +0545

    adds sql file in resources

--- resources/db/user.sql
+++ resources/db/user.sql
@@ -4,11 +4,11 @@ CREATE TABLE IF NOT EXISTS `user` (
   `id` int(12) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `type` enum('buyer','seller','','') NOT NULL,
+  `type` enum('buyer','seller') NOT NULL,
   `phone_no` bigint(20) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL,
   `is_deleted` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
