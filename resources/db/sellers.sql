669fb0f159d29e642a57fcd58bc8ee252ae8fbb9
pravinbashyal <pravin.bashyal@gmail.com>
2015-03-06 19:40:35 +0545

    clean up sql file

--- resources/db/sellers.sql
+++ resources/db/sellers.sql
@@ -1,31 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Mar 06, 2015 at 07:30 PM
--- Server version: 5.5.41
--- PHP Version: 5.3.10-1ubuntu3.16
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `mp_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sellers`
---
-
 CREATE TABLE IF NOT EXISTS `sellers` (
   `id` int(12) NOT NULL AUTO_INCREMENT,
   `user_id` int(12) NOT NULL,
@@ -39,16 +11,6 @@ CREATE TABLE IF NOT EXISTS `sellers` (
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `sellers`
---
 ALTER TABLE `sellers`
   ADD CONSTRAINT `sellers_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `sellers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- resources/db/stock.sql
+++ resources/db/stock.sql
@@ -1,31 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Mar 06, 2015 at 07:02 PM
--- Server version: 5.5.41
--- PHP Version: 5.3.10-1ubuntu3.16
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `mp_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `stock`
---
-
 CREATE TABLE IF NOT EXISTS `stock` (
   `id` int(12) NOT NULL AUTO_INCREMENT,
   `seller_id` int(12) NOT NULL,
@@ -41,16 +13,6 @@ CREATE TABLE IF NOT EXISTS `stock` (
   KEY `seller_id` (`seller_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `stock`
---
 ALTER TABLE `stock`
   ADD CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`seller_id`) REFERENCES `sellers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
