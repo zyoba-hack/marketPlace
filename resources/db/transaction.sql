479fb53a00fbbd40cef1865d0707b664cca81e9b
Sagar113 <SAGAR@sagaras-MacBook-Pro.local>
2015-03-06 20:04:38 +0545

    transaction sql changes

--- resources/db/transaction.sql
+++ resources/db/transaction.sql
@@ -1,9 +1,13 @@
-CREATE TABLE `mp_db`.`transaction` (
-`id` INT( 12 ) NOT NULL ,
-`seller_id` INT( 12 ) NOT NULL ,
-`buyer_id` INT( 12 ) NOT NULL ,
-`product_id` INT( 12 ) NOT NULL ,
-`is_deleted` ENUM( '1', '0' ) NOT NULL DEFAULT '0',
-`updated_at` TIMESTAMP NOT NULL ,
-`created_at` TIMESTAMP NOT NULL
-) ENGINE = InnoDB;
+DROP TABLE IF EXISTS `transaction` ;
+CREATE TABLE `transaction` (
+  `id` int(12) NOT NULL,
+  `seller_id` int(12) NOT NULL,
+  `buyer_id` int(12) NOT NULL,
+  `product_id` int(12) NOT NULL,
+  `is_deleted` enum('1','0') NOT NULL DEFAULT '0',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  KEY `seller_id` (`seller_id`),
+  KEY `buyer_id` (`buyer_id`),
+  KEY `product_id` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
