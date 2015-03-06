ee750a9d46e26b332851fbb1324cf3cc815cca7b
pravinbashyal <pravin.bashyal@gmail.com>
2015-03-06 19:31:15 +0545

    fix index issue: sellers.sql

--- resources/db/sellers.sql
+++ resources/db/sellers.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 06, 2015 at 05:20 PM
+-- Generation Time: Mar 06, 2015 at 07:30 PM
 -- Server version: 5.5.41
 -- PHP Version: 5.3.10-1ubuntu3.16
 
@@ -29,15 +29,25 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `sellers` (
   `id` int(12) NOT NULL AUTO_INCREMENT,
   `user_id` int(12) NOT NULL,
+  `contact_no` int(12) NOT NULL,
+  `address_line1` varchar(255) DEFAULT NULL,
+  `address_line2` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL,
   `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  KEY `user_id_2` (`user_id`),
-  KEY `user_id_3` (`user_id`),
-  KEY `updated_at` (`updated_at`)
+  KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `sellers`
+--
+ALTER TABLE `sellers`
+  ADD CONSTRAINT `sellers_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `sellers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
