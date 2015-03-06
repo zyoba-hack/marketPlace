8c1b62b0b23d80730eb14af991ff02fa118d6637
saurav <drklrd@drklrd.(none)>
2015-03-06 19:03:46 +0545

    adds product model and sql

--- models/market-place/index.js
+++ models/market-place/index.js
@@ -3,5 +3,6 @@ module.exports = {
   user: require('./user'),
   buyer: require('./buyer'),
   seller: require('./seller'),
-  stock: require('./stock')
+  stock: require('./stock'),
+  product : require ('./product')
 };
