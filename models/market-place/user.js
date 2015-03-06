906e3743b4b31c380a76e06d5c31809169ea9c0d
Suresh <suresh@zyoba.com>
2015-03-06 17:02:20 +0545

    adds functions in user model and adds database

--- models/market-place/user.js
+++ models/market-place/user.js
@@ -11,7 +11,7 @@ var bm = {
         columns: ['id', 'username', 'password', 'type', 'phone_no', 'created_at', 'updated_at', 'is_deleted']
     }),
 
-    fetch: function (filters, selectFields, cb) {
+    fetch: function fetch(filters, selectFields, cb) {
         if (typeof selectFields === 'function') {
             cb = selectFields;
             selectFields = null;
@@ -27,11 +27,62 @@ var bm = {
         query.exec(cb);
     },
 
-    all: function (cb) {
+    add: function add(data, cb) {
+        data.created_at = common.currentDate();
+        delete data.updated_at;
+        delete data.id;
+
+        var table = this.table;
+        var query = table.insert(data);
+        debug(query.toQuery());
+        query.exec(respond);
+
+        function respond(err, res) {
+            if (!err && res) {
+                data.id = res.insertId;
+                cb(null, data.id);
+            } else {
+                cb(err || new Error('unable to add new record'));
+            }
+        }
+    },
+
+    all: function all(cb) {
         this.fetch(null, cb);
     },
-    getPasswordByUser: function getPasswordByUser(id, cd) {
 
+    getPasswordByEmail: function getPasswordByEmail(username, cb) {
+        var filter = {
+            username: username
+        }
+        var selectFields = ['password'];
+        this.fetch(filter, selectFields, cb)
+    },
+
+
+    save: function save(data, cb) {
+        var keys = {};
+        if (data.id) keys.id = data.id;
+        if (!Object.keys(keys).length) {
+            return cb(new Error('keys not found'));
+        }
+
+        data.updated_at = common.currentDate();
+
+        var table = this.table;
+        var query = table.update(data);
+        common.generateWhereClause(table, query, keys);
+        debug(query.toQuery());
+        query.exec(respond);
+
+        function respond(err, res) {
+            if (!err && res) {
+                cb(null, data);
+            } else {
+                cb(err || new Error('unable to update record: ' + JSON.stringify(
+                    keys)));
+            }
+        }
     }
 };
 
@@ -40,11 +91,23 @@ module.exports = bm;
 //-- Test Code ----------------------------------------------------------
 if (require.main === module) {
     (function () {
-        console.log(require.main);
         // test code
-        bm.all(function (error, data) {
+        bm.getPasswordByEmail("masureshho@gmail.com", function (error, data) {
             console.log(error);
             console.log(data);
         });
     })();
 }
+// Update table
+/*if (require.main === module) {
+    (function () {
+        // test code
+        bm.save({
+            id: 1,
+            username: 'hello@there.com'
+        }, function (error, data) {
+            console.log(error);
+            console.log(data);
+        });
+    })();
+}*/
