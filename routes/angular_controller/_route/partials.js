a3585ac075cd5fe07381bbdd1589102bd257b4f9
Sagar113 <SAGAR@sagaras-MacBook-Pro.local>
2015-03-07 01:31:34 +0545

    set up for login

--- routes/angular_controller/_route/partials.js
+++ routes/angular_controller/_route/partials.js
@@ -1,7 +1,9 @@
 module.exports = function (app) {
   app.get('/views/buyers/partials/:urlname', function(req,res){
-  	console.log("hi");
   	res.render("buyers/partials/"+req.params.urlname);
+  });
+   app.get('/views/login/partials/:urlname', function(req,res){
+  	res.render("login/partials/"+req.params.urlname);
   });
   
  
---
+++ routes/mp-controller/login.js
@@ -0,0 +1,6 @@
+var login={
+	index:function(req,res,next){
+		res.render('login/partials/index')
+	}
+}
+module.exports=login;
