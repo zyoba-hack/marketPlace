f1b847617e9f5e38263fa12efb3dd13c2098b36c
pravinbashyal <pravin.bashyal@gmail.com>
2015-03-07 01:36:46 +0545

    fix merge conflict

--- routes/mp-controller/index.js
+++ routes/mp-controller/index.js
@@ -1,19 +1,9 @@
 var user = require('./user');
 var mw = require('../middleware');
-<<<<<<< HEAD
 var cg = require('./categories');
+var buyers=require('./buyers');
 module.exports = function (app) {
-
-  app.get('/', function(req, res, next) {
-  	res.render('buyers')
-  });
   app.get('/api/categories', cg.getList, mw.respond, mw.error);
-
-=======
-var buyers=require('./buyers')
-module.exports = function (app) {
-
   app.get('/login', user.collect, user.validate, user.render, mw.error);
   app.get('/buyer',buyers.index);
->>>>>>> 63f2d4bcc5f96016668358f95f57756bee4801ed
 }
