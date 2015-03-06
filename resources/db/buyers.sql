4b3d447461f0da0ab29a3e17087f08c2f6ae6605
Shankar Regmi <dell@dell.(none)>
2015-03-06 19:39:31 +0545

    adds fixes to buyers table schema

--- resources/db/buyers.sql
+++ resources/db/buyers.sql
@@ -13,5 +13,6 @@ CREATE TABLE IF NOT EXISTS `buyer` (
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `is_deleted` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
